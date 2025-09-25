7ac3daad86bdbf3f01656b43dc82d71484c7cb4b
iis <@iis-itsolution>
2025-09-25 13:11:58 +0300

    [refactor] Рефакторинг кода

--- inarctica_migration/functions/disk_migration.py
+++ inarctica_migration/functions/disk_migration.py
@@ -1,14 +1,14 @@
 from typing import Union
 
 from inarctica_migration.functions.helpers import retry_decorator
-from inarctica_migration.models import Group, User, Storage, Folder
+from inarctica_migration.models import Storage, Folder
 from inarctica_migration.utils import CloudBitrixToken, BoxBitrixToken
 from integration_utils.bitrix24.bitrix_token import BitrixToken
 
 
 @retry_decorator(attempts=3, delay=30)
 def _bx_storage_getlist(token: CloudBitrixToken | BoxBitrixToken) -> Union[list, dict]:
-    """"""
+    """Запрос disk.storage.getlist к REST API"""
 
     return token.call_list_method("disk.storage.getlist", timeout=100)
 
@@ -20,40 +20,45 @@ def _bx_folder_getchildren(
         filter: dict = None,
         select: list = None,
 ) -> Union[list, dict]:
-    """"""
-
+    """Запрос disk.folder.getchildren к REST API"""
     return token.call_list_method("disk.folder.getchildren", {"id": parent_id, "filter": filter, "select": select}, timeout=100)
 
 
-@retry_decorator(attempts=3, delay=30)
-def _bx_folder_get(
-        token: CloudBitrixToken,
-        filter: dict = None,
-        select: list = None,
-) -> Union[list, dict]:
-    """"""
+def _synchronize_storages(
+        cloud_token: CloudBitrixToken,
+        box_token: BoxBitrixToken
+) -> dict:
+    """
+    Функция ищет и записывает в БД связи между одинаковыми по названию хранилищами
 
-    return token.call_list_method("disk.folder.get")
-
-
-def _synchronize_storages(cloud_token: CloudBitrixToken, box_token: BoxBitrixToken):
-    """Создаём связи или обновляем их для одинаковых по названию хранилища"""
+    После завершения работы в бд появляются записи об одинаковых хранилищах: origin_id, destination_id
+    """
     bulk_data = []
+
+    storage_relation_map: dict[int, int] = dict(Storage.objects.all().values_list("origin_id", "destination_id"))
 
     try:
         current_cloud_storages = _bx_storage_getlist(cloud_token)
         current_box_storages = _bx_storage_getlist(box_token)
 
+        # Сравниваем все хранилища облака со всеми хранилищами коробки для нахождения идентичных
         for cloud_storage in current_cloud_storages:
             for box_storage in current_box_storages:
 
-                if cloud_storage["NAME"] == box_storage["NAME"]:
-                    bulk_data.append(
-                        Storage(
-                            origin_id=int(cloud_storage["ROOT_OBJECT_ID"]),
-                            destination_id=int(box_storage["ROOT_OBJECT_ID"]),
+                cloud_storage_obj_id = int(cloud_storage["ROOT_OBJECT_ID"])
+                box_storage_obj_id = int(box_storage["ROOT_OBJECT_ID"])
+
+                # Работаем только с несуществующими связями
+                if storage_relation_map.get(cloud_storage_obj_id) is None:
+                    if cloud_storage["NAME"] == box_storage["NAME"]:
+                        bulk_data.append(
+                            Storage(
+                                origin_id=cloud_storage_obj_id,
+                                destination_id=box_storage_obj_id,
+                            )
                         )
-                    )
+
+                        storage_relation_map[cloud_storage_obj_id] = box_storage_obj_id
 
     except Exception as exc:
         print(f"Произошла ошибка во время синхронизации хранилищ: {exc}")
@@ -67,20 +72,33 @@ def _synchronize_storages(cloud_token: CloudBitrixToken, box_token: BoxBitrixTok
 
         Storage.objects.bulk_create(
             unique_bulk_data,
+            batch_size=1000,
             unique_fields=["origin_id"],
             update_fields=["destination_id"],
             update_conflicts=True,
         )
 
-        print(f"Обработано (создано/обновлено) {len(unique_bulk_data)} связей между хранилищами")
-        return unique_bulk_data
+        print(f"Обработано (создано) {len(unique_bulk_data)} связей между хранилищами. Всего связей {len(storage_relation_map)}")
+        return storage_relation_map
 
 
-def _folders_recursive_descent(cloud_token: CloudBitrixToken, cloud_parent_id: int, result: Union[dict, None] = None):
+def _folders_recursive_descent(
+        cloud_token: CloudBitrixToken,
+        cloud_parent_id: int,
+        result: Union[dict, None] = None
+) -> dict[int, list]:
     """
-    Рекурсивно строит плоскую структуру вида:
-    {parent_id: [child_id1, child_id2, ...]}
+    Рекурсивно получает структуру, где ключи это (все дочерние папки для cloud_parent_id и сама эта папка с cloud_parent_id),
+    а значения это список всех дочерних папок для каждой из папок. Если папка не содержит в себе дочерней, то её список пуст []
+
+    {
+    cloud_parent_id: [child_id1, child_id2, ...],
+    parent1_id: [],
+    parent2_id: [],
+    }
     """
+
+    # Если дошли до конца, т.е у папки нет дочерних папок
     if result is None:
         result = {}
 
@@ -92,11 +110,11 @@ def _folders_recursive_descent(cloud_token: CloudBitrixToken, cloud_parent_id: i
             select=["ID", "PARENT_ID"]
         )
 
-        # добавляем прямых детей
+        # Добавляем прямых детей
         child_ids = [int(folder["ID"]) for folder in nested_folders]
         result[cloud_parent_id] = child_ids
 
-        # рекурсия для детей
+        # Рекурсия для детей
         for folder in nested_folders:
             folder_id = int(folder["ID"])
             _folders_recursive_descent(cloud_token, folder_id, result)
@@ -108,98 +126,112 @@ def _folders_recursive_descent(cloud_token: CloudBitrixToken, cloud_parent_id: i
         raise
 
 
-def _synchronize_folders_for_storage(cloud_token: CloudBitrixToken, box_token: BoxBitrixToken, cloud_storage_id: int, storage_relation_map: dict[int, int]):
-    """Создаём недостающие папки"""
+def _synchronize_folders_for_storage(
+        cloud_token: CloudBitrixToken,
+        box_token: BoxBitrixToken,
+        cloud_storage_id: int,
+        storage_relation_map: dict[int, int]
+):
+    """
+
+
+    """
     taking_methods = []
-    adding_methods = []
+    bulk_data = []
 
-    # folders_to_add: dict[str, list] = dict()
-
-    folders_relation_map: dict[int, int] = dict(Folder.objects.all().values_list("origin_id", "destination_id", ))
+    folders_relation_map: dict[int, int] = dict(Folder.objects.all().values_list("origin_id", "destination_id"))
 
     folder_tree_structure: dict = _folders_recursive_descent(cloud_token, cloud_storage_id)
     origin_folder_id_list: list[int] = list(folder_tree_structure.keys())
 
-    # Те которые мы создаём в процессе чтобы не грузить бд на каждом цикле
-    dynamic_created_folders: dict = folders_relation_map
     try:
-        #todo делать пропуск если в бд уже естановлена связь для foldов
+        # Забираем все найденные в хранилище папки (в т.ч. вложенные)
         for folder_id in origin_folder_id_list:
             taking_methods.append((str(folder_id), "disk.folder.get", {'id': folder_id}))
 
-        batch_result = cloud_token.batch_api_call(taking_methods)
+        all_cloud_storage_folders = cloud_token.batch_api_call(taking_methods)
 
-        for folder_id, folder_attributes in batch_result.successes.items():
+        # Для всех папок создаём подобные на бокс портале
+        for folder_id, folder_attributes in all_cloud_storage_folders.successes.items():
 
-            if not folder_attributes["result"]["PARENT_ID"]:
-                dynamic_created_folders[int(folder_id)] = storage_relation_map[int(folder_id)]
+            # Поведение если папка уже перенесенна и её связи записаны в бд
+            if int(folder_attributes["result"]["ID"]) in folders_relation_map:
                 continue
 
+            # Поведение при папка-хранилище (самой верхней папке в хранилище)
+            if not folder_attributes["result"]["PARENT_ID"]:
+                folders_relation_map[int(folder_id)] = storage_relation_map[int(folder_id)]
+                continue
+
+            # Поведение при всех вложенных папок
             else:
-                parent_id = dynamic_created_folders[int(folder_attributes["result"]["PARENT_ID"])]
+                cloud_parent_id = int(folder_attributes["result"]["PARENT_ID"])
+                box_parent_id = folders_relation_map[cloud_parent_id]
                 origin_folder_id_list = origin_folder_id_list[1:]
 
-            name = batch_result.successes[str(origin_folder_id_list[0])]['result']['NAME']
-            params = {"id": parent_id, "data": {"NAME": name}}
-            result = box_token.call_api_method("disk.folder.addsubfolder", params)
-            dynamic_created_folders[int(folder_id)] = result["result"]['ID']
+            name = all_cloud_storage_folders.successes[str(origin_folder_id_list[0])]['result']['NAME']
+            params = {"id": box_parent_id, "data": {"NAME": name}}
 
-            # if storage_relation_map.get(int(folder_id)):
-            #     origin_folder_id_list = origin_folder_id_list[1:]
-            #     name = batch_result.successes[str(origin_folder_id_list[0])]['result']['NAME']
-            #     origin_folder_id_list = origin_folder_id_list[1:]
-            #     params = {"id": storage_relation_map[int(folder_id)], "data": {"NAME": name}}
-            #
-            # else:
-            #     name = batch_result.successes[str(origin_folder_id_list[0])]['result']['NAME']
-            #     origin_folder_id_list = origin_folder_id_list[1:]
-            #     params = {"id": dynamic_created_folders[int(folder_id)], "data": {"NAME": name}}
-            #
-            # result = box_token.call_api_method("disk.folder.addsubfolder", params)
+            addsubfolder_result = box_token.call_api_method("disk.folder.addsubfolder", params)
+            added_box_folder_id = addsubfolder_result["result"]['ID']
 
-            # print(folder_id, {"id": storage_relation_map[folder_id], "data": {"NAME": folder_attributes['result']["NAME"]}})
-            # folders_to_add = []
-
-        return result
+            folders_relation_map[int(folder_id)] = added_box_folder_id
+            bulk_data.append(
+                Folder(
+                    origin_id=folder_id,
+                    destination_id=added_box_folder_id,
+                    parent_origin_id=cloud_parent_id,
+                    parent_destination_id=box_parent_id,
+                )
+            )
 
     except Exception as exc:
+        Folder.objects.bulk_create(
+            bulk_data,
+            unique_fields=["origin_id"],
+            update_fields=["destination_id", "parent_origin_id", "parent_destination_id"],
+            update_conflicts=True,
+        )
         print(f"Произошла ошибка в _synchronize_folders_for_storage (при синхронизации) : {exc}")
         raise
 
-    # for folder in folder_tree_structure:
-    #     ...
+    finally:
+        Folder.objects.bulk_create(
+            bulk_data,
+            unique_fields=["origin_id"],
+            update_fields=["destination_id", "parent_origin_id", "parent_destination_id"],
+            update_conflicts=True,
+        )
 
-    # return folder_tree_structure, origin_folder_list
-    # Сначала создать все папки которые существуют на облаке
-
-    # Затем пробежаться по их parent и расставить в правильном порядке
-
-
-def migrate_main(cloud_token: CloudBitrixToken, box_token: BoxBitrixToken):
-    """"""
-    #_recursive_descent(cloud_token, _MAIN_DISK_STORAGE_ID)
+        # len(folder_tree_structure) - 1 (Потому что мы не учитываем само хранилище, которое отдаётся нам как папка при запросе в битрикс)
+        print(f"Создано новых {len(bulk_data)} папок. Всего папок на в этом хранилище: {len(folder_tree_structure)-1}")
+        return
 
 
 def migrate_disk():
-    """"""
+    """
+    Пайплайн полной синхронизации дисков:
+    1) Настройка связей между хранилищами
+    2) Перенос структуры папок
+    3) Перенос файлов
+    """
+
     cloud_token = CloudBitrixToken()
     box_token = BoxBitrixToken()
 
-    _synchronize_storages(cloud_token=cloud_token, box_token=box_token)
-
+    # Настройка связей между хранилищами
     # {origin_id: destination_id, ...}
-    storage_relation_map: dict[int, int] = dict(Storage.objects.all().values_list("origin_id", "destination_id"))
+    storage_relation_map: dict[int, int] = _synchronize_storages(cloud_token=cloud_token, box_token=box_token)
 
+    # Воссоздание структуры папок для КАЖДОГО из хранилищ
     for storage_relation in storage_relation_map:
         #todo убрать хардкод (пока тестим на своих дисках)
-        if storage_relation != 25893:
+        if storage_relation != 1:
             continue
 
-        res = _synchronize_folders_for_storage(
+        _synchronize_folders_for_storage(
             cloud_token=cloud_token,
             box_token=box_token,
             cloud_storage_id=storage_relation,
             storage_relation_map=storage_relation_map,
         )
-
-        return res
--- inarctica_migration/models/disk.py
+++ inarctica_migration/models/disk.py
@@ -21,24 +21,3 @@ class Storage(models.Model):
 
     class Admin(admin.ModelAdmin):
         list_display = ('origin_id', 'destination_id')
-
-# from django.db import models
-# from mptt.models import MPTTModel, TreeForeignKey
-#
-# class Folder(MPTTModel):
-#     origin_id = models.PositiveIntegerField(primary_key=True)
-#     destination_id = models.PositiveIntegerField(blank=True, null=True)
-#     name = models.CharField(max_length=255, blank=True)
-#     origin_parent = TreeForeignKey(
-#         'self',
-#         on_delete=models.PROTECT,
-#         null=True,
-#         blank=True,
-#         related_name='children'
-#     )
-#     origin_parent_parent = TreeForeignKey()
-#     class MPTTMeta:
-#         order_insertion_by = ['name']
-#
-#     def __str__(self):
-#         return f"{self.name} ({self.bx_id})"
