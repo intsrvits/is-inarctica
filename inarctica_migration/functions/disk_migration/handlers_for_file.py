cd7d3b4c7896d48593cfd49b921b31b61b02e145
iis <@iis-itsolution>
2025-10-03 10:07:17 +0300

    [refact] Изменение логики под попытку обработки каждого файла хранилища

--- inarctica_migration/functions/disk_migration/handlers_for_file.py
+++ inarctica_migration/functions/disk_migration/handlers_for_file.py
@@ -1,5 +1,5 @@
 import requests
-import base64
+import pybase64
 
 from inarctica_migration.models import Folder, Storage
 from inarctica_migration.models.disk import File
@@ -10,16 +10,35 @@ from inarctica_migration.functions.disk_migration.bx_rest_requests import _bx_fo
 from inarctica_migration.functions.disk_migration.descent_by_recursion import ordered_hierarchy, recursive_descent, file_recursive_descent
 
 
-def get_file_content(
-        name: str,
-        url: str,
-) -> list:
-    response = requests.get(url)
-    response.raise_for_status()  # проверка на ошибки
+from inarctica_migration.utils.func_helpers import timer
 
-    # Преобразуем содержимое в base64
-    file_b64 = base64.b64encode(response.content).decode("utf-8")
+
+@timer
+def get_file_content(name: str, url: str) -> list[str]:
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
+
+        # создаём буфер для base64
+        encoder = pybase64.b64encode
+
+        # собираем все куски
+        chunks = []
+        chunks_cnt = 1
+        print('Получили респонс')
+        for chunk in response.iter_content(chunk_size=None):
+            print(f"Отработано {chunks_cnt} чанков")
+            chunks_cnt += 1
+
+            if chunk:  # пропускаем пустые keep-alive
+                chunks.append(encoder(chunk))
+                del chunk
+
+        # склеиваем в один base64-стринг
+        file_b64 = b"".join(chunks).decode("utf-8")
+        del chunks
+
     return [name, file_b64]
+
 
 def synchronize_files_for_storage(
         cloud_token: CloudBitrixToken,
@@ -30,12 +49,23 @@ def synchronize_files_for_storage(
     # Для ведения статистик
     storage = Storage.objects.get(cloud_id=cloud_storage_id)
     box_storage_files_cnt = storage.files_in_box
-
-    cloud_files_cnt=0
-    uploaded_files_cnt=0
+    uploaded_files_cnt = 0
 
     bulk_data = []
 
+    # 0. Получаем структуру узла и всех закрепленных за ним файлов
+    folder_and_files = file_recursive_descent(
+        token=cloud_token,
+        object_type="file",
+        cloud_parent_id=cloud_storage_id,
+    )
+
+    all_cloud_files_cnt = 0
+    for file_in_folder in folder_and_files.values():
+        all_cloud_files_cnt += len(file_in_folder)
+
+    storage.files_in_cloud = all_cloud_files_cnt
+    storage.save()
 
     # 1. Сливаем две модели для получения карты связей папок и хранилищ
     storage_relation_map: dict[int, int] = dict(Storage.objects.all().values_list("cloud_id", "box_id"))
@@ -43,36 +73,35 @@ def synchronize_files_for_storage(
     merged_relation_map = {**storage_relation_map, **folders_relation_map}
 
     files_relation_map: dict[int, int] = dict(File.objects.all().values_list("cloud_id", "box_id"))
+    folders = Folder.objects.all()
 
-    # 2. Получаем структуру узла и всех закрепленных за ним файлов
-    folder_and_files: dict[int, list[tuple]] = file_recursive_descent(
-        token=cloud_token,
-        object_type="file",
-        cloud_parent_id=cloud_storage_id,
-    )
-    try:
-        for folder, files in folder_and_files.items():
-            folder_id = int(folder)
-            if folder_id not in merged_relation_map:
-                continue
+    for folder, files in folder_and_files.items():
+        folder_id = int(folder)
+        if folder_id not in merged_relation_map:
+            continue
 
-            box_folder_id = merged_relation_map[folder_id]
+        box_folder_id = merged_relation_map[folder_id]
 
-            for file in files:
-                cloud_files_cnt += 1
-
+        for file in files:
+            try:
                 file_cloud_id = int(file[0])
                 file_name = file[1]
                 file_url = file[2]
+                file_byte_size = int(file[3])
+
+                # if file_byte_size > 111137542421:
+                #     # todo убрать
+                #     continue
 
                 if file_cloud_id in files_relation_map:
                     continue
 
+                file_content = get_file_content(file_name, file_url)
                 params = {
-                        'id': box_folder_id,
-                        'data': {'NAME': file_name},
-                        'fileContent': get_file_content(file_name, file_url),
-                        'generateUniqueName': True
+                    'id': box_folder_id,
+                    'data': {'NAME': file_name},
+                    'fileContent': file_content,
+                    'generateUniqueName': True
                 }
 
                 uploadfile_result = _bx_folder_uploadfile(box_token, params)
@@ -89,23 +118,34 @@ def synchronize_files_for_storage(
 
                 uploaded_files_cnt += 1
 
-    except Exception as exc:
-        debug_point(
-            "❌ Произошла ошибка при переносе файлов synchronize_files_for_storage\n"
-            f"boxID={box_folder_id}\t boxFileId={uploaded_file_box_id}"
-            f"https://bitrix24.inarctica.com/bitrix/tools/disk/focus.php?folderId={box_folder_id}&action=openFolderList&ncc=1\n\n"
-            f"cloudID={cloud_storage_id}\n cloudFileId={file_cloud_id}"
-            f"https://inarctica.bitrix24.ru/bitrix/tools/disk/focus.php?folderId={cloud_storage_id}&action=openFolderList&ncc=1\n\n"
-            f"{exc}",
-        )
-        raise
+            except MemoryError:
+                debug_point(
+                    "❌ Не хватает памяти на перенос файла\n"
+                    f"{locals().get('file_name'), file_byte_size}"
+                    f"cloudID={cloud_storage_id}\n cloudFileId={file_cloud_id}\n"
+                    f"boxID={box_folder_id}\n\n"
+                )
+                # можно continue, чтобы пропустить проблемный файл
+                continue
 
-    finally:
-        storage.files_in_cloud = cloud_files_cnt
+            except Exception as exc:
+                debug_point(
+                    "❌ Ошибка при переносе файла\n"
+                    f"{locals().get('file_name'), file_byte_size}"
+                    f"boxID={box_folder_id}\n boxFileId={locals().get('uploaded_file_box_id')}\n"
+                    f"cloudID={cloud_storage_id}\n cloudFileId={file_cloud_id}\n"
+                    f"{exc}"
+                )
+                # пропускаем файл и идём дальше
+                continue
+
         storage.files_in_box += uploaded_files_cnt
 
         if storage.files_in_cloud == storage.files_in_box:
             storage.files_sync = True
+
+        else:
+            storage.files_sync = False
 
         File.objects.bulk_create(
             bulk_data,
@@ -127,9 +167,3 @@ def synchronize_files_for_storage(
                     f"https://inarctica.bitrix24.ru/bitrix/tools/disk/focus.php?folderId={cloud_storage_id}&action=openFolderList&ncc=1\n\n",
                     )
         return
-
-
-
-
-
-
