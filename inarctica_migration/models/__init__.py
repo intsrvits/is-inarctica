5ff810714dc03b4578a5a4f78402f22722eb062d
iis <@iis-itsolution>
2025-10-15 17:39:23 +0300

    [feat] Добавление модели TaskMigration

--- inarctica_migration/models/__init__.py
+++ inarctica_migration/models/__init__.py
@@ -3,3 +3,4 @@ from .disk import Folder, Storage
 from .group import Group
 from .user import User
 from .log import LogMigration
+from .task import TaskMigration
---
+++ inarctica_migration/models/task.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.db import models
+
+
+class TaskMigration(models.Model):
+    """Модель переноса задач"""
+    cloud_id = models.IntegerField("ID на облаке", unique=True)
+    box_id = models.IntegerField("ID на коробке", blank=True, null=True)
+
+    group_is_sync = models.BooleanField("Группа синхронизована", default=False)
+
+    cloud_parent_id = models.IntegerField("ID родительской задачи на коробке", blank=True, null=True)
+    box_parent_id = models.IntegerField("ID родительской задачи на коробке", blank=True, null=True)
+
+    is_synced = models.BooleanField("Запись синхронизирована", default=False)
+
+    class Admin(admin.ModelAdmin):
+        list_display = ("cloud_id", "box_id", "group_is_sync", "cloud_parent_id", "box_parent_id", "is_synced")
