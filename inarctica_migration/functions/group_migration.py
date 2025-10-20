c3bc9314d1d251a34923eab20adb665e0341c211
iis <@iis-itsolution>
2025-10-20 12:02:52 +0300

    [fix] Добавлен учёт групп с секретным типом приватности

--- inarctica_migration/functions/group_migration.py
+++ inarctica_migration/functions/group_migration.py
@@ -1,12 +1,6 @@
 from inarctica_migration.models import Group, User
 from inarctica_migration.utils import CloudBitrixToken, BoxBitrixToken
 
-SELECT_FIELDS = ("ID", "ACTIVE", "SITE_ID", "SUBJECT_ID", "SUBJECT_DATA", "NAME", "DESCRIPTION", "KEYWORDS", "CLOSED", "VISIBLE",
-                 "OPENED", "PROJECT", "LANDING", "DATE_CREATE", "DATE_UPDATE", "DATE_ACTIVITY", "IMAGE_ID", "AVATAR", "AVATAR_TYPE", "AVATAR_TYPES",
-                 "OWNER_ID", "OWNER_DATA", "NUMBER_OF_MEMBERS", "NUMBER_OF_MODERATORS", "INITIATE_PERMS", "PROJECT_DATE_START",
-                 "PROJECT_DATE_FINISH", "SEARCH_INDEX", "SCRUM_OWNER_ID", "SCRUM_MASTER_ID", "SCRUM_SPRINT_DURATION",
-                 "SCRUM_TASK_RESPONSIBLE", "TYPE", "UF_SG_DEPT", "TAGS", "ACTIONS", "USER_DATA")
-
 
 def _create_groups(cloud_token: CloudBitrixToken, box_token: BoxBitrixToken):
     """Создаёт группы, которые есть на cloud-портале, но нет на box-портале"""
@@ -18,57 +12,33 @@ def _create_groups(cloud_token: CloudBitrixToken, box_token: BoxBitrixToken):
         group_origin_destination_map: dict[int, int] = dict(Group.objects.all().values_list("origin_id", "destination_id"))
         user_origin_destination_map: dict[int, int] = dict(User.objects.all().values_list("origin_id", "destination_id"))
 
-        params_to_select = {
-            "IS_ADMIN": "Y",
-            "select": [
-                "ID",
-                "NAME",
-                "DESCRIPTION"
-                "VISIBLE",
-                "OPENED",
-                "KEYWORDS",
-                "INITIATE_PERMS",
-                "CLOSED",
-                "SPAM_PERMS",
-                "PROJECT",
-                "PROJECT_DATE_FINISH",
-                "PROJECT_DATE_START",
-                "SCRUM_MASTER_ID",
-                "OWNER_ID",
-            ]
-        }
+        origin_groups = cloud_token.call_list_method("sonet_group.get", {"IS_ADMIN": "Y"})
+        destination_groups = box_token.call_list_method("sonet_group.get", {"IS_ADMIN": "Y"})
 
-        origin_groups = cloud_token.call_list_method("socialnetwork.api.workgroup.list", params_to_select)["workgroups"]
-        destination_groups = box_token.call_list_method("socialnetwork.api.workgroup.list", params_to_select)["workgroups"]
-
-        box_group_ids = set(int(group["id"]) for group in destination_groups)
+        box_group_ids = set(int(group["ID"]) for group in destination_groups)
 
         for group in origin_groups:
-            _SCRUM_MASTER_ID = None
 
-            if group_origin_destination_map.get(int(group["id"])) not in box_group_ids:
-
-                if group.get("scrumMasterId"):
-                    _SCRUM_MASTER_ID = user_origin_destination_map[int(group.get("scrumMasterId"))]
+            if group_origin_destination_map.get(int(group["ID"])) not in box_group_ids:
 
                 params_to_migrate = {
-                    "NAME": group.get("name"),
-                    "DESCRIPTION": group.get("description"),
-                    "VISIBLE": group.get("visible"),
-                    "OPENED": group.get("opened"),
-                    "KEYWORDS": group.get("keywords"),
-                    "INITIATE_PERMS": group.get("initiatePerms"),
-                    "CLOSED": group.get("closed"),
-                    "SPAM_PERMS": group.get("spamPerms"),
-                    "PROJECT": group.get("project"),
-                    "PROJECT_DATE_FINISH": group.get("projectDateFinish"),
-                    "PROJECT_DATE_START": group.get("projectDateStart"),
-                    "OWNER_ID": user_origin_destination_map.get(int(group["ownerId"]), 1),
-                    "SCRUM_MASTER_ID": _SCRUM_MASTER_ID,
+                    "NAME": group.get("NAME"),
+                    "DESCRIPTION": group.get("DESCRIPTION"),
+                    "VISIBLE": group.get("VISIBLE"),
+                    "OPENED": group.get("OPENED"),
+                    "KEYWORDS": group.get("KEYWORDS"),
+                    "INITIATE_PERMS": "A",
+                    "ACTIVE": group.get("ACTIVE"),
+                    "PROJECT": group.get("PROJECT"),
+                    "CLOSED": group.get("CLOSED"),
+                    "OWNER_ID": user_origin_destination_map.get(int(group["OWNER_ID"]), 1),
                 }
 
-                destination_id = box_token.call_api_method("sonet_group.create", params_to_migrate)["result"]
-                bulk_data.append(Group(origin_id=int(group["id"]), destination_id=destination_id))
+                try:
+                    destination_id = box_token.call_api_method("sonet_group.create", params_to_migrate)["result"]
+                    bulk_data.append(Group(origin_id=int(group["ID"]), destination_id=destination_id))
+                except Exception as exc:
+                    print(f"Ошибка при создании с параметрами {params_to_migrate}: {exc}")
 
         Group.objects.bulk_create(
             bulk_data,
