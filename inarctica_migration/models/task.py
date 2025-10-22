960cfc5dd21ff535ac456b0875d33e21adec8be6
iis <@iis-itsolution>
2025-10-22 11:10:42 +0300

    [feat] подготовка к миграции стадий канбана

--- inarctica_migration/models/task.py
+++ inarctica_migration/models/task.py
@@ -17,3 +17,18 @@ class TaskMigration(models.Model):
 
     class Admin(admin.ModelAdmin):
         list_display = ("cloud_id", "box_id", "box_group_id", "group_is_sync", "cloud_parent_id", "box_parent_id", "is_synced")
+
+
+class stageMigration(models.Model):
+    """"""
+
+    cloud_id = models.IntegerField("ID на облаке", blank=True, null=True)
+    box_id = models.IntegerField("ID на коробке", blank=True, null=True)
+
+    cloud_group_id = models.IntegerField("ID группы на облаке", blank=True, null=True)
+    box_group_id = models.IntegerField("ID группы на коробке", blank=True, null=True)
+
+    is_synced = models.BooleanField("Стадия синхронизирована", default=False)
+
+    class Admin(admin.ModelAdmin):
+        list_display = ("cloud_id", "box_id", "cloud_group_id", "box_group_id")
