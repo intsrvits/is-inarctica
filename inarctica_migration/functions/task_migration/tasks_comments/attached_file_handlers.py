814a69e550b1f3a67cc096ea4462892d5346a1a9
iis <@iis-itsolution>
2025-10-29 19:11:32 +0300

    ✨ feat: added file content fetching and migration to task comments module

--- inarctica_migration/functions/task_migration/tasks_comments/attached_file_handlers.py
+++ inarctica_migration/functions/task_migration/tasks_comments/attached_file_handlers.py
@@ -1,3 +1,11 @@
+import re
+import pybase64
+import requests
+
+from inarctica_migration.functions.disk_migration.bx_rest_requests import bx_disk_storage_uploadFile
+from inarctica_migration.utils import BoxBitrixToken
+
+
 def check_attachments_in_comment(comment) -> list:
     """Возвращает список прикрепленных к комментарию файлов."""
     attached_files_data = []
@@ -18,3 +26,53 @@ def check_attachments_in_comment(comment) -> list:
             })
 
     return attached_files_data
+
+
+def get_file_content(name: str, url: str) -> list[str]:
+    url = 'https://inarctica.bitrix24.ru' + url
+
+    with requests.get(url, stream=True) as response:
+        response.raise_for_status()
+
+        # создаём буфер для base64
+        encoder = pybase64.b64encode
+
+        # собираем все куски
+        chunks = []
+        chunks_cnt = 1
+        print('Получили респонс')
+        for chunk in response.iter_content(chunk_size=None):
+            print(f"Отработано {chunks_cnt} чанков")
+            chunks_cnt += 1
+
+            if chunk:  # пропускаем пустые keep-alive
+                chunks.append(encoder(chunk))
+                del chunk
+
+        # склеиваем в один base64-стринг
+        file_b64 = b"".join(chunks).decode("utf-8")
+        del chunks
+
+    return [name, file_b64]
+
+
+def migrate_attached_files(token: BoxBitrixToken, storage_id: int, files: list, ):
+    attachment_file_id_map = dict()
+    for file in files:
+        name = file["NAME"]
+        url = file["URL"]
+        cloud_attachment_file_id = int(file["FILE_ID"])
+        file_content = get_file_content(name, url)
+
+        params_to_upload = {
+            "id": storage_id,
+            "fileContent": file_content,
+            "data": {"NAME": name},
+            "generateUniqueName": True,
+        }
+
+        box_attachment_file_id = int(bx_disk_storage_uploadFile(token, params_to_upload)["result"]["ID"])
+        attachment_file_id_map[cloud_attachment_file_id] = box_attachment_file_id
+
+    return attachment_file_id_map
+
