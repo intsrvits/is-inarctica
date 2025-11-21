a461ff0f48a7ace672ff2d62944a8038d73a0eba
iis <@iis-itsolution>
2025-11-21 18:02:35 +0300

    ✨ refactor: removed legacy blogpost migration functions in `migration_scenario.py`

--- inarctica_migration/functions/log_migration/migration_scenario.py
+++ inarctica_migration/functions/log_migration/migration_scenario.py
@@ -1,3 +1,5 @@
+from typing import Dict, Tuple, List
+
 from inarctica_migration.functions.log_migration.debug_messages import error_log_message, success_log_message
 from inarctica_migration.models import LogMigration, User, Group
 from inarctica_migration.utils import CloudBitrixToken, BoxBitrixToken
@@ -10,186 +12,6 @@ from inarctica_migration.functions.log_migration.handlers import (
     get_files_base64,
     get_files_links, clean_title, clean_detail_text,
 )
-
-
-def common_log_migration():
-    """"""
-    bulk_data = []
-
-    cloud_token = CloudBitrixToken()
-    box_token = BoxBitrixToken()
-
-    existed_logs: dict[int, bool] = dict(LogMigration.objects.all().values_list("cloud_id", "is_synced"))
-    migrated_users: dict[int, int] = dict(User.objects.all().values_list("origin_id", "destination_id"))
-
-    # Получаем список всех облачных постов, отсортированный по дате созданию
-    sorted_cloud_blogposts = get_sorted_by_time_blogposts(cloud_token)
-    sorted_cloud_blogposts_map = {int(blogpost["ID"]): blogpost for blogpost in sorted_cloud_blogposts}
-
-    # Обновляем список известных постов
-    existed_logs = init_blogpost_into_db(sorted_cloud_blogposts_map, existed_logs)
-
-    try:
-        # Обрабатываем все найденные посты
-        for cloud_blogpost_id, is_synced in existed_logs.items():
-
-            # Если уже синхронизовано, то пропускаем
-            if is_synced:
-                continue
-
-            current_blogpost = sorted_cloud_blogposts_map[int(cloud_blogpost_id)]
-            cloud_author_id = current_blogpost["AUTHOR_ID"]
-            box_author_id = migrated_users.get(int(cloud_author_id), 1)
-
-            params = {
-                "USER_ID": box_author_id,
-                "POST_MESSAGE": clean_detail_text(current_blogpost["DETAIL_TEXT"]),
-                "POST_TITLE": clean_title(current_blogpost["DETAIL_TEXT"], current_blogpost["TITLE"]),
-                "DEST": ["UA"]
-            }
-
-            # Если обнаружены прикрепленные файлы в облачном посте, то подготавливаем их к переносу
-            if current_blogpost.get("FILES"):
-                try:
-                    files_attributes: list[tuple[str, str]] = get_files_links(cloud_token, current_blogpost["FILES"])
-
-                    params["FILES"] = get_files_base64(files_attributes)
-                except:
-                    pass
-
-            migration_result = bx_log_blogpost_add(box_token, params)
-
-            bulk_data.append(
-                LogMigration(
-                    cloud_id=int(current_blogpost["ID"]),
-                    box_id=int(migration_result["result"]),
-                    dest="UA",
-                    is_synced=True,
-                )
-            )
-
-    except Exception as exc:
-        debug_point(error_log_message(exc, cloud_blogpost_cnt=len(sorted_cloud_blogposts), box_blogpost_cnt=192 + len(bulk_data), migrated_now=len(bulk_data)))
-
-    finally:
-
-        LogMigration.objects.bulk_create(
-            bulk_data,
-            batch_size=1000,
-            unique_fields=["cloud_id"],
-            update_fields=["box_id", "dest", "is_synced"],
-            update_conflicts=True,
-        )
-
-        debug_point(success_log_message(cloud_blogpost_cnt=len(sorted_cloud_blogposts), box_blogpost_cnt=192 + len(bulk_data), migrated_now=len(bulk_data)))
-
-
-def _migrate_blogposts_for_feed(
-        existed_logs: dict[int, bool],
-        migrated_users: dict[int, int],
-        migrated_groups: dict[int, int],
-        cloud_group_id: int = None,
-) -> dict[int, bool]:
-    """"""
-
-    dest = f"SG{cloud_group_id}" if cloud_group_id else None
-
-    bulk_data = []
-
-    cloud_token = CloudBitrixToken()
-    box_token = BoxBitrixToken()
-
-    current_group = Group.objects.filter(origin_id=cloud_group_id).first()
-
-    migrated_blogpost_cnt = 0
-    if not current_group.blogposts_cnt:
-        current_group.blogposts_cnt = 0
-
-    # Получаем список всех облачных постов, отсортированный по дате созданию
-    sorted_cloud_blogposts = get_sorted_by_time_blogposts(cloud_token, dest=cloud_group_id)
-    sorted_cloud_blogposts_map = {int(blogpost["ID"]): blogpost for blogpost in sorted_cloud_blogposts}
-
-    if len(sorted_cloud_blogposts) == 0:
-        current_group.save()
-        debug_point(success_log_message(cloud_blogpost_cnt=len(sorted_cloud_blogposts), box_blogpost_cnt=current_group.blogposts_cnt, migrated_now=0, cloud_dest=cloud_group_id, box_dest=migrated_groups[cloud_group_id]))
-
-        return existed_logs
-
-    # Обновляем список известных постов
-    existed_logs = init_blogpost_into_db(sorted_cloud_blogposts_map, existed_logs, dest)
-
-    try:
-        # Обрабатываем все найденные посты
-        for cloud_blogpost_id, current_blogpost in sorted_cloud_blogposts_map.items():
-            if not existed_logs[int(cloud_blogpost_id)]:
-
-                cloud_author_id = current_blogpost["AUTHOR_ID"]
-                box_author_id = migrated_users.get(int(cloud_author_id), 1)
-
-                params = {
-                    "USER_ID": box_author_id,
-                    "POST_MESSAGE": clean_detail_text(current_blogpost["DETAIL_TEXT"]),
-                    "POST_TITLE": clean_title(current_blogpost["DETAIL_TEXT"], current_blogpost["TITLE"]),
-                    "DEST": [f"SG{migrated_groups[cloud_group_id]}"]
-                }
-
-                # Если обнаружены прикрепленные файлы в облачном посте, то подготавливаем их к переносу
-                if current_blogpost.get("FILES"):
-                    files_attributes: list[tuple[str, str]] = get_files_links(cloud_token, current_blogpost["FILES"])
-
-                    params["FILES"] = get_files_base64(files_attributes)
-
-                migration_result = bx_log_blogpost_add(box_token, params)
-
-                bulk_data.append(
-                    LogMigration(
-                        cloud_id=int(current_blogpost["ID"]),
-                        box_id=int(migration_result["result"]),
-                        dest=dest if dest else "UA",
-                        is_synced=True,
-                    )
-                )
-
-        current_group.blogposts_cnt += len(bulk_data)
-        debug_point(success_log_message(cloud_blogpost_cnt=len(sorted_cloud_blogposts), box_blogpost_cnt=current_group.blogposts_cnt, migrated_now=len(bulk_data), cloud_dest=cloud_group_id, box_dest=migrated_groups[cloud_group_id]))
-
-    except Exception as exc:
-        debug_point(error_log_message(exc, cloud_blogpost_cnt=len(sorted_cloud_blogposts), box_blogpost_cnt=current_group.blogposts_cnt, migrated_now=len(bulk_data), cloud_dest=cloud_group_id, box_dest=migrated_groups[cloud_group_id]))
-
-    finally:
-
-        LogMigration.objects.bulk_create(
-            bulk_data,
-            batch_size=1000,
-            unique_fields=["cloud_id"],
-            update_fields=["box_id", "dest", "is_synced"],
-            update_conflicts=True,
-        )
-
-        current_group.save()
-
-        return existed_logs
-
-
-def migrate_blogposts_for_portals():
-    """"""
-
-    existed_logs: dict[int, bool] = dict(LogMigration.objects.all().values_list("cloud_id", "is_synced"))
-    migrated_users: dict[int, int] = dict(User.objects.all().values_list("origin_id", "destination_id"))
-    migrated_groups: dict[int, int] = dict(Group.objects.filter(id__gt=59).values_list("origin_id", "destination_id"))
-
-    # Перенос для постов в лентах групп
-    for cloud_group_id in migrated_groups:
-
-        existed_logs = _migrate_blogposts_for_feed(
-            existed_logs=existed_logs,
-            migrated_users=migrated_users,
-            migrated_groups=migrated_groups,
-            cloud_group_id=cloud_group_id,
-        )
-
-    debug_point("Миграция групповых новостных лент окончена")
-
 
 def left_from_all_groups():
     """"""
@@ -216,16 +38,88 @@ def join_in_all_groups():
     return batch_result
 
 
-def migrate_common_blogposts():
-    existed_logs: dict[int, bool] = dict(LogMigration.objects.all().values_list("cloud_id", "is_synced"))
-    migrated_users: dict[int, int] = dict(User.objects.all().values_list("origin_id", "destination_id"))
-    migrated_groups: dict[int, int] = dict(Group.objects.all().values_list("origin_id", "destination_id"))
+def get_all_blogposts_by_dest(token: CloudBitrixToken, group_ids) -> Dict[str, Dict]:
+    """Возвращает словарь, где ключ это S{group_id} или UA, а значение словарь с блог постами"""
+    all_blogposts: Dict[str, Dict] = dict()
+    for group_id in group_ids:
+        group_blogposts = token.call_list_method("log.blogpost.get", {"LOG_RIGHTS": [f"SG{group_id}"]})
+        if group_blogposts:
+            all_blogposts[str(group_id)] = group_blogposts
 
-    existed_logs = _migrate_blogposts_for_feed(
-        existed_logs=existed_logs,
-        migrated_users=migrated_users,
-        migrated_groups=migrated_groups,
-    )
+    ua_blogposts = token.call_list_method("log.blogpost.get", {"LOG_RIGHTS": "UA"})
+    all_blogposts["UA"] = ua_blogposts
 
-    debug_point("Миграция групповых новостных лент окончена")
-    return existed_logs
+    return all_blogposts
+
+
+def get_structure_by_blogpost_ids(all_blogposts_by_dest):
+    """Вернет словарь, где ключ это blogpost ID, а значение это кортеж из blogpost_data и месте назначения т.е группа или UA"""
+    result = dict()
+    blogpost_in_groups = []
+    for group_id, blogposts in all_blogposts_by_dest.items():
+        if group_id == "UA":
+            continue
+
+        for blogpost in blogposts:
+            result[int(blogpost["ID"])] = (group_id, blogpost)
+            blogpost_in_groups.append(int(blogpost["ID"]))
+
+    ua_blogposts = all_blogposts_by_dest["UA"]
+    for blogpost in ua_blogposts:
+        if int(blogpost["ID"]) in blogpost_in_groups:
+            continue
+
+        result[int(blogpost["ID"])] = ("UA", blogpost)
+
+    return result
+
+
+def migrate_blogposts():
+    cloud_token = CloudBitrixToken()
+    box_token = BoxBitrixToken()
+
+    migrated_users_map: Dict[int, int] = dict(User.objects.all().values_list("origin_id", "destination_id"))
+    migrated_group_map: Dict[int, int] = dict(Group.objects.all().values_list("origin_id", "destination_id"))
+    migrated_blogposts_ids: List[int] = list(LogMigration.objects.filter(is_synced=True).values_list("cloud_id", flat=True))
+
+    all_blogposts_by_dest: Dict[str, dict] = get_all_blogposts_by_dest(cloud_token, migrated_group_map.keys())
+    prepared_blog_posts: Dict[int, Tuple[int, Dict]] = get_structure_by_blogpost_ids(all_blogposts_by_dest)
+
+    for blogpost_id in sorted(prepared_blog_posts.keys()):
+        if blogpost_id in migrated_blogposts_ids:
+            continue
+
+        current_blogpost = prepared_blog_posts[blogpost_id][1]
+        cloud_dest = prepared_blog_posts[blogpost_id][0]  # Либо "UA", либо ID группы
+
+        cloud_author_id = current_blogpost["AUTHOR_ID"]
+        box_author_id = migrated_users_map.get(int(cloud_author_id), 1)
+
+        if cloud_dest == "UA":
+            dest = "UA"
+        else:
+            dest = f"SG{migrated_group_map[cloud_dest]}"
+
+        params = {
+            "USER_ID": box_author_id,
+            "POST_MESSAGE": clean_detail_text(current_blogpost["DETAIL_TEXT"]),
+            "POST_TITLE": clean_title(current_blogpost["DETAIL_TEXT"], current_blogpost["TITLE"]),
+            "DEST": [dest],
+        }
+
+        # Если обнаружены прикрепленные файлы в облачном посте, то подготавливаем их к переносу
+        if current_blogpost.get("FILES"):
+            files_attributes: list[tuple[str, str]] = get_files_links(cloud_token, current_blogpost["FILES"])
+            params["FILES"] = get_files_base64(files_attributes)
+
+        migrated_blogpost_box_id = int(bx_log_blogpost_add(box_token, params)["result"])
+        LogMigration.objects.create(
+            cloud_id=blogpost_id,
+            box_id=migrated_blogpost_box_id,
+            dest=dest,
+            is_synced=True,
+        )
+
+    debug_point("Миграция блогпостов окончена\n"
+                f"На облаке {len(prepared_blog_posts)}"
+                f"На коробке {len(LogMigration.objects.all().count())}")
