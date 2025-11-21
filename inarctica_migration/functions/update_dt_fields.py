327ab4d355acb2356457480da2a2e429aecc8596
iis <@iis-itsolution>
2025-11-21 15:25:41 +0300

    ✨ feat: restored and updated `update_comments_dt` in `update_dt_fields.py`

--- inarctica_migration/functions/update_dt_fields.py
+++ inarctica_migration/functions/update_dt_fields.py
@@ -2,6 +2,7 @@ from typing import Dict, List
 
 from django.utils.dateparse import parse_datetime
 
+from b24.models.b_forum_message import BForumMessage
 from inarctica_migration.functions.helpers import debug_point
 from inarctica_migration.functions.log_migration.handlers import get_sorted_by_time_blogposts
 from inarctica_migration.models import LogMigration, CommentMigration, TaskMigration
@@ -43,37 +44,36 @@ def update_blogposts_dt():
                 f" Всего на коробке: {len(box_posts)}")
 
 
-#
-# def update_comments_dt():
-#     """Обновляем дату создания полей в бд, в соответствии с хронологическим порядок на облачном портале"""
-#
-#     cloud_token = CloudBitrixToken()
-#
-#     comments_map: Dict[int, int] = CommentMigration.objects.filter(is_synced=True).values_list("box_id", "cloud_id")
-#     all_cloud_tasks: List[int] = list(TaskMigration.objects.filter(is_synced=True).values_list("cloud_id", flat=True))
-#
-#     #db_comments = BForumMessage.objects.all()
-#     db_comments_map = {db_comment.id: db_comment for db_comment in db_comments}
-#     for cloud_task_id in all_cloud_tasks:
-#         try:
-#             comments_to_updating = get_comments_to_migration(cloud_token, cloud_task_id)
-#             comment_ids_in_chrono: list[int] = sorted(comments_to_updating.keys())
-#
-#             bulk_data = []
-#             for comment_id in comment_ids_in_chrono:
-#                 current_comment = comments_to_updating[comment_id]
-#
-#                 post_date = current_comment["POST_DATE"]
-#                 commet_box_id = comments_map.get(comment_id)
-#                 if commet_box_id:
-#                     processed_comment = db_comments_map[commet_box_id]
-#                     processed_comment.post_date = post_date
-#                     processed_comment.save()
-#                     debug_point("Обновлён комментарий с box_id = ")
-#
-#         except Exception as exc:
-#             debug_point("Произошла ошибка при обновлении dt-полей комментариев\n"
-#                         f"CLOUD TASK ID: {cloud_task_id}"
-#                         f"CLOUD COM ID: {comment_id}"
-#                         )
-#
+def update_comments_dt():
+    """Обновляем дату создания полей в бд, в соответствии с хронологическим порядок на облачном портале"""
+    bulk_data = []
+
+    all_cloud_tasks: List[int] = list(TaskMigration.objects.filter(is_synced=True).values_list("cloud_id", flat=True))
+    comments_map: Dict[int, int] = dict(CommentMigration.objects.values_list("box_id", "cloud_id"))
+
+    cloud_token = CloudBitrixToken()
+    all_cloud_comments_by_id: Dict[int, dict] = dict()
+    for cloud_task_id in all_cloud_tasks:
+        task_comments: Dict[int, dict] = get_comments_to_migration(cloud_token, cloud_task_id)
+        all_cloud_comments_by_id |= task_comments
+
+    skipped = 0
+    box_comments = BForumMessage.objects.using("shhtunnel_db").all()
+    for comment in box_comments:
+        cloud_comment_id = comments_map.get(comment.id, 0)
+        if cloud_comment_id not in all_cloud_comments_by_id:
+            skipped += 1
+            continue
+
+        dt = parse_datetime(all_cloud_comments_by_id[cloud_comment_id]["POST_DATE"])
+
+        comment.post_date = dt
+        comment.edit_date = dt
+
+        bulk_data.append(comment)
+
+    BForumMessage.objects.using("shhtunnel_db").bulk_update(bulk_data, ["post_date", "edit_date"])
+    debug_point(f"Обновлено {len(bulk_data)} блогпостов."
+                f" Всего на облаке: {len(all_cloud_comments_by_id)}."
+                f" Всего на коробке: {len(comments_map)} BForumMessage {len(box_comments)}\n"
+                f" Пропущено = {skipped}")
