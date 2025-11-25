946904849d1be154a2667916f0cdb00ea701c028
iis <@iis-itsolution>
2025-11-25 11:15:06 +0300

    ✨ feat: added debug_point call and batch_api_call timeout in `task_update.py`

--- inarctica_migration/functions/task_migration/task_update.py
+++ inarctica_migration/functions/task_migration/task_update.py
@@ -1,5 +1,6 @@
 from typing import Dict
 
+from inarctica_migration.functions.helpers import debug_point
 from inarctica_migration.functions.task_migration.bx_rest_request import bx_tasks_task_list
 from inarctica_migration.functions.task_migration.constants import CLOUD_TASK_USERFIELDS, TASK_USERFIELDS_MAP, BOX_TASK_USERFIELDS
 from inarctica_migration.models import TaskMigration
@@ -8,21 +9,17 @@ from inarctica_migration.utils import BoxBitrixToken, CloudBitrixToken
 
 def update_task():
     """Обновляет пользовательские поля для перенесенных задач"""
-    get_methods = []
     update_methods = []
-
-    box_token = BoxBitrixToken()
-    migrated_tasks: Dict[int, int] = dict(TaskMigration.objects.filter(is_synced=True).values_list("cloud_id", "box_id"))
 
     cloud_token = CloudBitrixToken()
     box_token = BoxBitrixToken()
-    migrated_tasks: Dict[int, int] = dict(TaskMigration.objects.filter(is_synced=True).values_list("cloud_id", "box_id"))
 
+    migrated_tasks: Dict[int, int] = dict(TaskMigration.objects.filter(is_synced=True).values_list("cloud_id", "box_id"))
     all_cloud_tasks = bx_tasks_task_list(cloud_token, params={"select": ["ID", *list(CLOUD_TASK_USERFIELDS.keys())]})
 
     for cloud_task in all_cloud_tasks:
         box_task_id = migrated_tasks.get(int(cloud_task["id"]))
-        if not box_task_id:
+        if not box_task_id or box_task_id != 3209:
             continue
         params = {"taskId": box_task_id, "fields": dict()}
         for cloud_task_userfield, box_task_userfield in TASK_USERFIELDS_MAP.items():
@@ -30,5 +27,9 @@ def update_task():
 
         update_methods.append((str(box_task_id), "task.item.update", params))
 
-    update_batch_result = box_token.batch_api_call(update_methods)
+    update_batch_result = box_token.batch_api_call(update_methods, timeout=25)
+    debug_point("Обновление пользовательских полей в задачах\n"
+                f"Всего задач: {len(all_cloud_tasks)}\n"
+                f"Обновлено: {len(update_batch_result.successes)}\n"
+                f"C ошибкой: {len(update_batch_result.errors)}\n")
     return update_batch_result
